bfce17e32e25edbc172d39248579fa243701847a
pabloyanezb <60148155+pabloyanezb@users.noreply.github.com>
2020-03-05 13:31:21 -0300

    Especificaciones básicas cumplidas
    1.La aplicación está dividida en 3 secciones (cabecera, sección para agregar una nueva tarea y la lista de tareas a agregar.
    2.La cabecera es fija al igual que la sección para agregar tareas nuevas, la única sección que tiene un scrollbar son las secciones donde se despliegan las tareas agregadas.
    3.El usuario debe poder escribir un texto de más de una línea (textarea) y una vez que presione el botón agregar este debe agregar la tarea a la lista
    4.No debe poderse agregar un texto en blanco
    5.El checkbox en la tarea debe permitir tachar la tarea de la lista
    6.El icono que aparece a la derecha de cada tarea debe ser un tacho de borrar (font-awesome) y al hacerle click debemos poder eliminar la tarea de la lista.

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
-$('.btn-secondary').on('click', function (){
-    var x = $('textarea').val(); 
-    $('.panel').html('<div class="tarea">'+x+'</div>');
+$(document).ready(function(){
+    $('.btn-secondary').on('click', function (){
+        var tarea = $('textarea').val();
+        if (tarea == ''){
+            return;
+        }
+        $('.list').append('<li class="tarea"><input type="checkbox"><span>'
+                            +tarea+ 
+                            '</span><a href="#"></a></li>'
+                        );
+        $('textarea').val('');
+    })
+    $(document).on('click', 'a', function(){
+        $(this).parent().slideUp(100,function(){
+            $(this).remove();
+        });
+    })
+    $(document).on('change', 'input:checkbox', function(){
+        var input = $(this).next('span');
+        if (this.checked) {
+            $(input).css('textDecoration', 'line-through');
+        } else {
+            $(input).css('textDecoration', 'none');
+        }
+    })
 })
+
